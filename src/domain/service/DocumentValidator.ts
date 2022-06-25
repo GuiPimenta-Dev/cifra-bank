1ea43f3d58e1056b8553a1361a7c3659d3abdd0a
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2022-06-25 15:19:07 -0300

    style: updating cpf to value on DocumentValidator.ts

--- src/domain/service/DocumentValidator.ts
+++ src/domain/service/DocumentValidator.ts
@@ -1,15 +1,15 @@
 export default abstract class DocumentValidator {
-  protected clean(cpf: string): string {
-    return cpf.replace(/\D/g, "");
+  protected clean(value: string): string {
+    return value.replace(/\D/g, "");
   }
 
-  protected hasCorrectLength(cpf: string, length: number) {
-    return cpf.length === length;
+  protected hasCorrectLength(value: string, length: number) {
+    return value.length === length;
   }
 
-  protected isBlocked(cpf: string) {
-    const [firstDigit] = cpf;
-    return [...cpf].every((digit) => digit === firstDigit);
+  protected isBlocked(value: string) {
+    const [firstDigit] = value;
+    return [...value].every((digit) => digit === firstDigit);
   }
 
   abstract calculateDigit(value: string, factor: number): number;
